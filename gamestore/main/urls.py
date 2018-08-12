cb2abb33134e49b3aae5b9f149cf88776d5644f4
Timothy Kavuma <Timo@localhost.localdomain>
2018-08-12 10:12:29 +0300

    commit two

--- gamestore/main/urls.py
+++ gamestore/main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from . import views
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 from .forms import AuthenticationForm
-from . import views
+
 
 urlpatterns = [
     path(r'', views.index, name='index'),
-    path(r'accounts/login/', login, {'template_name': 'login.html','authentication_form': AuthenticationForm}, name='login'),
-    path(r'accounts/logout/', logout, {'next_page': '/'}, name='logout'),
+    path(r'accounts/login/', login, {'template_name': 'login.html', 'authentication_form': AuthenticationForm }, name='login'),
+    path(r'accounts/logout/', logout, {'next_page': '/' }, name='logout'),
+    path(r'accounts/signup/', views.signup, name='signup'),
 ]
